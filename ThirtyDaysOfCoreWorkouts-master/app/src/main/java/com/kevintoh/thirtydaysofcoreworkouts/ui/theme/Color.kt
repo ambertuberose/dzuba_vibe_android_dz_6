78fc09984f94d85f33ae0280a29e200836a76c8e
ambertuberose <160414065+ambertuberose@users.noreply.github.com>
2024-03-03 02:47:20 +0300

    30DayApp

---
+++ ThirtyDaysOfCoreWorkouts-master/app/src/main/java/com/kevintoh/thirtydaysofcoreworkouts/ui/theme/Color.kt
@@ -0,0 +1,22 @@
+package com.kevintoh.thirtydaysofcoreworkouts.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Light Theme
+// Light Theme
+val md_theme_light_background = Color(0xFFFFEBF3)
+val md_theme_light_surface = Color(0xFFFFCDD2)
+val md_theme_light_on_surface = Color(0xFF2A2A2A)
+val md_theme_light_primary = Color(0xFFFFC0CB)
+val md_theme_light_on_primary = Color(0xFF3D3D3D)
+val md_theme_light_secondary = Color(0xFFFF4081)
+val md_theme_light_on_secondary = Color(0xFFFFFFFF)
+
+// Dark Theme
+val md_theme_dark_background = Color(0xFF7B1FA2)
+val md_theme_dark_surface = Color(0xFFD81B60)
+val md_theme_dark_on_surface = Color(0xFFFFFFFF)
+val md_theme_dark_primary = Color(0xFFC2185B)
+val md_theme_dark_on_primary = Color(0xFFFFFFFF)
+val md_theme_dark_secondary = Color(0xFF880E4F)
+val md_theme_dark_on_secondary = Color(0xFFFFFFFF)
